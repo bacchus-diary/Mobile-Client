a55661943aaa6ab779f14e1fc86ea460bbaa1480
Kunio Sawatani <devel@fathens.org>
2016-02-10 12:59:23 +0900

    Leaf を description で検索するクラスを作成

---
+++ dart/lib/service/leaves.dart
@@ -0,0 +1,26 @@
+library bacchus_diary.service.leaves;
+
+import 'package:logging/logging.dart';
+
+import 'package:bacchus_diary/model/report.dart';
+import 'package:bacchus_diary/service/aws/dynamodb.dart';
+import 'package:bacchus_diary/service/reports.dart';
+import 'package:bacchus_diary/util/pager.dart';
+
+final _logger = new Logger('Leaves');
+
+class Leaves {
+  static Pager<Leaf> byDescription(String desc) {
+    final map = new ExpressionMap();
+
+    final nameContent = map.putName('CONTENT');
+    final nameDesc = map.putName('description');
+    final valueDesc = map.putValue(desc);
+
+    final exp1 = "${nameContent}.${nameDesc} CONTAINS ${valueDesc}";
+    final exp2 = "${valueDesc} CONTAINS ${nameContent}.${nameDesc}";
+    final exp = [exp1, exp2].join(' OR ');
+
+    return Reports.TABLE_LEAF.scanPager(exp, map.names, map.values);
+  }
+}
