05e6ec140a15aab269809b8db22f04bb0d93e32b
Kunio Sawatani <devel@fathens.org>
2016-02-12 10:22:16 +0900

    不要なクラスを削除

--- dart/lib/util/enums.dart
+++
@@ -1,4 +0,0 @@
-library bacchus_diary.util.enums;
-
-String nameOfEnum(e) => e == null ? null : e.toString().split("\.").last;
-enumByName(List values, String name) => name == null ? null : values.firstWhere((a) => nameOfEnum(a) == name);
